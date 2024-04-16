d86288d79b8f3802866d142edd953e1704ef9059
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-04-16 18:06:26 -0300

    configuracao inicial e crud curso

---
+++ src/models/Professor.js
@@ -0,0 +1,10 @@
+// const { DataTypes } = require('sequelize')
+// const { connection } = require('../database/connection')
+
+// const Professor = connection.define('professores', {
+//     nome: {
+//         type: DataTypes.STRING,
+//     },
+   
+
+// module.exports = Professor
---
+++ src/routes/routes.js
@@ -0,0 +1,80 @@
+const {Router} = require('express');
+
+const routes = new Router()
+
+
+routes.get('/bem_vindo', (req, res) => {
+    res.json('Bem vindo');
+    })
+
+module.exports = routes;
+
+routes.post('/cursos', async (req, res) => {
+    try {
+        const nome = req.body.nome
+        const duracao_horas = req.body.duracao_horas
+
+        if(!nome) {
+            return res.status(400).json({messagem: "O nome é obrigatório" })
+        }
+
+        if(!(duracao_horas >= 40 && duracao_horas <= 200)) {
+            return res.status(400).json({  messagem: "A duração do curso deve ser entre 40 e 200 horas"
+            })
+        }
+
+        const curso = await Curso.create({
+            nome: nome,
+            duracao_horas: duracao_horas
+        })
+
+        res.status(201).json(curso)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível cadastrar o curso' })
+    }
+
+})
+
+routes.get('/cursos', async (req, res) => {
+    let params = {}
+
+    if(req.query.nome)  {
+        params = {...params, nome: req.query.nome}
+    }
+
+    const cursos = await Curso.findAll({
+        where: params
+    })
+
+    res.json(cursos)
+})
+
+routes.put('/cursos/:id', async (req, res) => {
+    const id = req.params.id
+
+    const curso = await Curso.findByPk(id)
+
+    if(!curso) {
+        return res.status(404).json({mensagem: 'Curso não encontraddo'})
+    }
+    curso.update(req.body)
+
+    await curso.save()
+
+    res.json(curso)
+})
+
+routes.delete('/cursos/:id', (req,res) => {
+    const {id} =  req.params
+
+    Curso.destroy({
+        where: {
+            id: id
+        }
+    }) // DELETE cursos from cursos where id = 1
+  
+    res.status(204).json({})
+})
+
+module.exports = routes
